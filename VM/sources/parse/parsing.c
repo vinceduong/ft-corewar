3dabcfd3c5173787417d3375d6f3aacbb52d6abb
Theo ESCRIVA <thescriv@student.42.fr>
2018-11-13 14:29:17 +0100

    WoW

--- VM/sources/parse/parsing.c
+++ VM/sources/parse/parsing.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thescriv <thescriv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/31 15:50:56 by thescriv          #+#    #+#             */
-/*   Updated: 2018/11/13 14:24:40 by thescriv         ###   ########.fr       */
+/*   Created: 2018/11/13 14:28:42 by thescriv          #+#    #+#             */
+/*   Updated: 2018/11/13 14:29:04 by thescriv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/vm.h"
+#include "vm.h"
 
-int ft_start(char **av, t_vm *vm)
+int ft_parser(char **av, t_vm *vm)
 {
-	if (parser(av, vm) == 0)
-		return (0);
-}
-
-int main(int ac, char **av)
-{
-	t_vm vm;
-
-	if (ac <= 3)
-	{
-		if (ft_start(av, &vm) == 0)
-			ft_error(1);
-	}
-	else
-		ft_error(1);
-	return (0);
+	
 }
