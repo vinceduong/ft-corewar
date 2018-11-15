ecd4263988ce113485eeca02ff94b764cc316675
Theo ESCRIVA <thescriv@student.42.fr>
2018-11-15 12:52:40 +0100

    ChEcK_ArGs In ProGreSs

--- VM/sources/parse/parsing.c
+++ VM/sources/parse/parsing.c
@@ -6,30 +6,30 @@
 /*   By: thescriv <thescriv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 14:28:42 by thescriv          #+#    #+#             */
-/*   Updated: 2018/11/14 18:43:35 by thescriv         ###   ########.fr       */
+/*   Updated: 2018/11/15 12:30:47 by thescriv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-int parser(char **av, t_vm *vm)
+int ft_parser(t_vm *vm, char **av)
 {
 	int error;
 
-	if ((error = check_args(av)) < 0)
+	if ((error = check_args(av, vm)) < 0)
 	{
-		printf("check_args failed - Error Type = %d \n");
+		printf("check_args failed - Error Type = %d \n", 1);
 		return (error);
 	}
-	if ((error = fill_flags(av)) < 0)
+	/*if ((error = fill_flags(av)) < 0)
 	{
-		printf("fill_flags failed - Error Type = %d \n");	
+		printf("fill_flags failed - Error Type = %d \n");
 		return (error);
 	}
 	if ((error = fill_players(av, vm)) < 0)
 	{
-		printf("fill_players failed - Error Type = %d \n");	
-		return (error);		
-	}
+		printf("fill_players failed - Error Type = %d \n");
+		return (error);
+	}*/
 	return (1);
 }
--- VM/sources/start.c
+++ VM/sources/start.c
@@ -6,15 +6,15 @@
 /*   By: thescriv <thescriv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 18:38:02 by thescriv          #+#    #+#             */
-/*   Updated: 2018/11/14 18:42:45 by thescriv         ###   ########.fr       */
+/*   Updated: 2018/11/15 12:25:44 by thescriv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-int ft_start(char **av, t_vm *vm)
+int ft_start(t_vm *vm, char **av)
 {
-	if (ft_parser(av, vm) == 0)
+	if (ft_parser(vm, av) == 0)
 		return (0);
 	/*if (ft_init_map(vm) == 0)
 		return (0);
