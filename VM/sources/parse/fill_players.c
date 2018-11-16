920ec98adde047d4c1325b1199ee24ad3395789e
Vincent DUONG <vinceduong@hotmail.fr>
2018-11-16 16:50:20 +0100

    fill_players update

--- VM/sources/parse/fill_players.c
+++ VM/sources/parse/fill_players.c
@@ -12,33 +12,29 @@
 
 #include "vm.h"
 
-int get_info(t_player *info, char *file)
+int fill_player(t_player *player, char *file)
 {
-	//int ret;
 	int fd;
 
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
+	if ((fd = open(file, O_RDONLY) < 0)
 		return (-1);
-	/*ret = read(fd, &info->header, sizeof(t_header));
-	printf("%d %lu\n", ret, sizeof(t_header));*/
 	printf("%lu\n", sizeof(t_header));
-	info->header = malloc(sizeof(t_header));
-	if (read(fd, info->header, sizeof(t_header)) != sizeof(t_header))
+	player->header = malloc(sizeof(t_header));
+	if (read(fd, player->header, sizeof(t_header)) != sizeof(t_header))
 		return (-1);
 	printf("OK2\n");
-	if (swap_int(info->header->magic) != COREWAR_EXEC_MAGIC)
+	if (swap_int(player->header->magic) != COREWAR_EXEC_MAGIC)
 		return (-1);
 	printf("OK3\n");
-	info->header->prog_size = swap_uint(info->header->prog_size);
-	info->header->prog_name[PROG_NAME_LENGTH] = '\0';
-	info->header->comment[COMMENT_LENGTH] = '\0';
+	player->header->prog_size = swap_uint(player->header->prog_size);
+	player->header->prog_name[PROG_NAME_LENGTH] = '\0';
+	player->header->comment[COMMENT_LENGTH] = '\0';
 	printf("OK4\n");
-	if (info->header->prog_size > CHAMP_MAX_SIZE)
+	if (player->header->prog_size > CHAMP_MAX_SIZE)
 		return (-1);
 	printf("OK5\n");
-	if (read(fd, &info->prog, info->header->prog_size)
-	!= info->header->prog_size)
+	if (read(fd, &player->prog, player->header->prog_size)
+	!= player->header->prog_size)
 		return (-1);
 	printf("OK6\n");
 	close(fd);
@@ -56,7 +52,7 @@ int fill_players(char **av, t_vm *vm)
 		printf("HAHAHHAHAHA\n");
 		if (vm->players[i].p)
 		{
-			if (get_info(&vm->players[i], vm->players[i].filename) == -1)
+			if (fill_player(&vm->players[i], vm->players[i].filename) == -1)
 				return (-1);
 		}
 		i++;
