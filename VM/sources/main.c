6e08859fb1d6d5d9613b1dbda577a5bc6264f04b
Vincent DUONG <vinceduong@hotmail.fr>
2018-11-20 11:55:26 +0100

    init_ram and fill_ram added

--- VM/sources/main.c
+++ VM/sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thescriv <thescriv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vduong <vduong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 15:50:56 by thescriv          #+#    #+#             */
-/*   Updated: 2018/11/20 11:03:30 by thescriv         ###   ########.fr       */
+/*   Updated: 2018/11/20 11:41:51 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ int main(int ac, char **av)
 	init_vm(&vm);
 	if (ac < 2 || !parse(&vm, av))
 		error(USAGE);
-	//else
-		//ft_exit(&vm, 0);
+	init_ram(&vm);
+	printf("init_ram succedd\n");
+
 	return (0);
 }
