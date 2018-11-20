5b74120bfad1dc8b418f6d6cf04935c3fbc5b882
Alize ETCHEGORRY <aetchego@e3r8p15.42.fr>
2018-11-20 18:17:14 +0100

    vm.h changed

--- VM/sources/loop/loop.c
+++ VM/sources/loop/loop.c
@@ -6,19 +6,21 @@
 /*   By: vduong <vduong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 13:33:41 by vduong            #+#    #+#             */
-/*   Updated: 2018/11/20 17:28:12 by thescriv         ###   ########.fr       */
+/*   Updated: 2018/11/20 17:51:05 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "vm.h"
 
 void loop(t_vm *vm)
 {
+    int step;
+
+    step = 1;
     while (1)
     {
         if (vm->flag.visu)
-            /*display(vm)*/;
+            display(vm, step);
         else if (vm->cycle == vm->flag.dump)
             dump(vm);
         vm->cycle++;
@@ -26,5 +28,6 @@ void loop(t_vm *vm)
         vm->check_cycles--;
         if (!vm->check_cycles && !check_stack(vm))
             break;
+        step = 2;
     }
 }
