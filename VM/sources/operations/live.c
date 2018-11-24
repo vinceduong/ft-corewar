deaa76696b06377ff13aa99ffd32dc57f8670824
Vincent Duong <vinceduong@hotmail.fr>
2018-11-24 10:40:51 +0100

    printf replaced by ft_printf

--- VM/sources/operations/live.c
+++ VM/sources/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: vduong <vduong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 12:01:37 by thescriv          #+#    #+#             */
-/*   Updated: 2018/11/24 09:21:38 by vduong           ###   ########.fr       */
+/*   Updated: 2018/11/24 10:40:26 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void live(t_vm *vm, t_proc *proc, t_operation *ope)
 		{
 			vm->players[ope->param[0] - 1].nb_live++;
 			vm->winner = ope->param[0] - 1;
-			printf("The player \"%s\" ($%d) is alive !\n",
+			ft_printf("The player \"%s\" ($%d) is alive !\n",
 				vm->players[ope->param[0] - 1].filename, proc->player);
 		}
 	}
