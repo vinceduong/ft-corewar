a1fb602d9e6f8c0e709b3d73e5c074baf981c189
Vincent Duong <vinceduong@hotmail.fr>
2018-11-27 20:47:16 +0100

    glow added

--- VM/sources/process/init_process.c
+++ VM/sources/process/init_process.c
@@ -6,7 +6,7 @@
 /*   By: vduong <vduong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 12:18:09 by vduong            #+#    #+#             */
-/*   Updated: 2018/11/25 13:25:39 by tescriva         ###   ########.fr       */
+/*   Updated: 2018/11/27 20:44:02 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ t_proc		*init_process(t_vm *vm, int pc, int player)
     new->id = id++;
     new->pc = pc;
     new->opcode = vm->ram[pc].content;
+	vm->ram[pc].glow = 1;
     new->cycle = new->opcode > 0 && new->opcode <= 16 ?
 		op_tab[(int)new->opcode - 1].cycles : 1;
     new->player = player;
