1b7776c9988b5e3b5e59fc27b6d419b54c20cdfb
Vincent DUONG <vinceduong@hotmail.fr>
2018-11-29 16:36:40 +0100

    visu merging

--- VM/sources/loop/loop.c
+++ VM/sources/loop/loop.c
@@ -6,7 +6,7 @@
 /*   By: vduong <vduong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 13:33:41 by vduong            #+#    #+#             */
-/*   Updated: 2018/11/29 15:37:47 by vduong           ###   ########.fr       */
+/*   Updated: 2018/11/29 16:36:35 by vduong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,8 @@ void		loop(t_vm *vm)
             break;
         step++;
     }
-<<<<<<< HEAD
-	dump(vm);
-    printf("J'ai fait %d cycles \nJe suis a %d cycle_to_die\n", vm->cycles_total, vm->cycles_to_die);
-=======
     if (vm->flag.visu)
         display_winner(&vm->display);
 	//dump(vm);
   //  printf("J'ai fait %d cycles \nJe suis a %d cycle_to_die\n", vm->cycles_total, vm->cycles_to_die);
->>>>>>> visu
 }
